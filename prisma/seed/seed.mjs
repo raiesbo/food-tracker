539392ded57c59037c0cd7e2905f2502f54f5632
raiesbo <raiesbo@gmail.com>
2023-03-31 22:52:46 +0200

    extend seedding data

--- prisma/seed/seed.mjs
+++ prisma/seed/seed.mjs
@@ -10,7 +10,13 @@ async function main() {
             email: 'alice@prisma.io',
             firstName: 'Alice',
             lastName: 'Muller',
-            role: 'CUSTOMER'
+            role: 'CUSTOMER',
+            resturants: {
+                create: [
+                    { name: 'Alices Burguer', description: 'Best burger in town!' },
+                    { name: 'Alices Sushi', description: 'Best Sushi in town!' }
+                ]
+            }
         },
     })
 
@@ -25,7 +31,46 @@ async function main() {
         },
     })
 
-    console.log({ alice, bob })
+    const mainUser = await prisma.user.findUnique({ where: { email: 'raiesbo8@gmail.com' } });
+
+    const categorySpanish = await prisma.category.create({ data: { name: 'Spanish', createdById: mainUser.id } });
+    const categoryGerman = await prisma.category.create({ data: { name: 'German', createdById: mainUser.id } });
+
+    const restaurantBurger = await prisma.restaurant.create({
+        data: {
+            name: 'Main Burguer',
+            description: 'Top burger in town!',
+            userId: mainUser.id,
+            categoryId: categorySpanish.id
+        }
+    })
+
+    const restaurantSushi = await prisma.restaurant.create({
+        data: {
+            name: 'Second Sushi',
+            description: 'Second best Sushi in town!',
+            userId: mainUser.id,
+            categoryId: categoryGerman.id
+        }
+    })
+
+    const locationBerlin = await prisma.location.create({
+        data: {
+            country: "German",
+            city: 'Berlin',
+            streetName: 'some street',
+            restaurantId: restaurantBurger.id
+        }
+    })
+
+    const locationMunich = await prisma.location.create({
+        data: {
+            country: "German",
+            city: 'Munich',
+            streetName: 'some other street',
+            restaurantId: restaurantSushi.id
+        }
+    })
 }
 main()
     .then(async () => {
