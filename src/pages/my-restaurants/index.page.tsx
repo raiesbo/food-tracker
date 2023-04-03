8ccc9c54f0e87a35494b612e9d60ec1125705796
raiesbo <raiesbo@gmail.com>
2023-04-03 19:02:00 +0200

    style my-restaurants page

--- src/pages/my-restaurants/index.page.tsx
+++ src/pages/my-restaurants/index.page.tsx
@@ -4,9 +4,10 @@ import { paths } from "@/utils/paths";
 import { auth0Config } from "@/utils/settings";
 import { getSession } from "@auth0/nextjs-auth0";
 import AddIcon from '@mui/icons-material/Add';
-import { Card, CssBaseline, IconButton } from "@mui/material";
+import { Card, CssBaseline } from "@mui/material";
 import { Restaurant } from "@prisma/client";
 import { GetServerSidePropsContext } from "next";
+import Image from "next/image";
 import Link from "next/link";
 import styles from './myRestaurants.module.scss';
 
@@ -43,6 +44,8 @@ type Props = {
     restaurants: Array<Restaurant>
 }
 
+const imagePlaceholder = 'https://images.unsplash.com/photo-1570441262582-a2d4b9a916a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2048&q=80';
+
 export default function MyRestaurants({ restaurants }: Props) {
 
     return (
@@ -50,23 +53,49 @@ export default function MyRestaurants({ restaurants }: Props) {
             <NavigationMenu />
             <main className={styles.root}>
                 <CssBaseline />
-                <div>
-                    My Restaurants Page
-                </div>
-                <div>
+                <h1>
+                    My Restaurants
+                </h1>
+                <div className={styles.listContainer}>
                     {restaurants.map(restaurant => (
-                        <Card key={restaurant.id}>
-                            <p>{restaurant.name}</p>
-                            <p>{restaurant.description}</p>
+                        <Card
+                            key={restaurant.id}
+                            className={styles.card}
+                        >
+                            <Link
+                                href={'/my-restaurants/' + restaurant.id}
+                                className={styles.cardLink}
+                            >
+                                <div className={styles.imageContainer}>
+                                    <Image
+                                        src={imagePlaceholder}
+                                        alt='Restaurant image'
+                                        fill
+                                        className={styles.backgroundImage}
+                                    />
+                                </div>
+                                <div className={styles.cardText}>
+                                    <h3 className={styles.name}>
+                                        {restaurant.name}
+                                    </h3>
+                                    <p className={styles.description}>
+                                        {restaurant.description}
+                                    </p>
+                                </div>
+                            </Link>
                         </Card>
                     ))}
-                    <div>
-                        <Link href={paths.createNewRestaurant}>
-                            <IconButton>
-                                <AddIcon />
-                            </IconButton>
+                    <Card
+                        className={styles.card}
+                        title='Create a new Restaurant'
+                    >
+                        <Link
+                            href={paths.createNewRestaurant}
+                            className={styles.cardLinkPlus}
+                        >
+                            <AddIcon />
                         </Link>
-                    </div>
+                    </Card>
                 </div>
             </main>
         </>
