1ae0422dfa5ba8c1c29d73598f48b44953b9e828
raiesbo <raiesbo@gmail.com>
2023-04-08 19:15:01 +0200

    implement information section component

---
+++ src/components/InfoSection/InfoSection.tsx
@@ -0,0 +1,42 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { IconButton } from '@mui/material';
+import cc from 'classcat';
+import { ReactNode, useState } from 'react';
+import { Text } from '../Text';
+import styles from './InfoSection.module.scss';
+
+type Props = {
+    children: ReactNode,
+    title: string,
+    className?: string,
+    childrenClassName?: string,
+}
+
+export default function InfoSection({ children, title, className, childrenClassName }: Props) {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    return (
+        <section className={cc([styles.root, className])}>
+            <header className={styles.header}>
+                <Text as='h2' variant='h3' bold>
+                    {title}
+                </Text>
+                <IconButton size='small'
+                    className={cc([isCollapsed && styles.rotateChevron])}
+                    onClick={() => setIsCollapsed(!isCollapsed)}
+                >
+                    <ExpandMoreIcon
+
+                    />
+                </IconButton>
+            </header>
+            <div className={cc([
+                styles.sectionContent,
+                isCollapsed && styles.sectionContent_collapsed,
+                childrenClassName
+            ])}>
+                {children}
+            </div>
+        </section>
+    )
+}
