700f36a86db7b377a4bcdd5311ce7382d3915ba9
raiesbo <raiesbo@gmail.com>
2023-04-29 01:09:03 +0200

    fix tests

--- src/utils/formatDate.test.tsx
+++ src/utils/formatDate.test.tsx
@@ -1,23 +1,26 @@
 import formatDate from './formatDate';
-
-jest.useFakeTimers().setSystemTime(new Date('2023-04-28'));
+import { expect, test, describe, beforeEach, vi } from "vitest";
 
 describe('formatDate', () => {
-	it('returns the correct date for today', () => {
+	beforeEach(() => {
+		vi.setSystemTime(new Date('2023-04-28'));
+	});
+
+	test('returns the correct date for today', () => {
 		const date = new Date();
 		const laterToday = date.setHours(date.getHours() - 3);
-		expect(formatDate(laterToday)).toBe('heute 21:00');
+		expect(formatDate(laterToday)).toBe('today 21:00');
 	});
 
-	it('returns the correct date for yesterday', () => {
+	test('returns the correct date for yesterday', () => {
 		const date = new Date();
 		const tomorrow = date.setHours(date.getHours() - 28);
-		expect(formatDate(tomorrow)).toBe('gestern 20:00');
+		expect(formatDate(tomorrow)).toBe('yesterday 20:00');
 	});
 
-	it('returns the correct date for more than one day of difference', () => {
+	test('returns the correct date for more than one day of difference', () => {
 		const date = new Date();
 		const tomorrow = date.setDate(date.getDate() - 3);
-		expect(formatDate(tomorrow)).toBe('25.04.2023, 00:00');
+		expect(formatDate(tomorrow)).toBe('25/04/2023, 00:00');
 	});
 });
